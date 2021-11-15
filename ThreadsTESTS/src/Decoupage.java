72f35b2e4c687fee43e6ca946f2bd604c4bfe07e
ZiedGOBJI <zied.gobji@protonmail.ch>
2021-11-15 11:18:12 +0100

    ss

--- ThreadsTESTS/src/Decoupage.java
+++ ThreadsTESTS/src/Decoupage.java
@@ -1,21 +1,11 @@
 package lib;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
-import java.util.ListIterator;
-import java.util.Scanner;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Decoupage {
@@ -83,8 +73,6 @@ public class Decoupage {
 		          
 		          // Création des chaines de caractères (parties de textes que chaque thread va traiter)
 		          
-		          int valeur_max = intervales.get(intervales.size()-1); // dernière valeur de 'intervales'
-		          
 		          List<List<String>> chaines = new ArrayList<List<String>>(); // Liste principale, contenant les sous-listes 'chaine'
 		          
 		          List<String> clef_suppression = new ArrayList<>();
@@ -92,59 +80,47 @@ public class Decoupage {
 		          clef_suppression.add("-------");
 		          
 		          
-		          int compteur =0;
+		          
 		          
 		          
 		          // ERREUR JY SUIS PRESQUE
 		        
-		          for (int q=0; q<2*nb_thread-1 ; q++) { 		        	  
-		        	  List<String> chaine = new ArrayList<>(); // sous-liste
-		        	  System.out.println("thread");
+		          intervales.set(intervales.size()-1, words.size());
+		         System.out.println("intervales mis à jour : " + intervales);
+		          
+		         
+		         
+		          for (int q=0; q<intervales.size()-1 ; q++) { 
 		        	  
+		        	  if (intervales.get(q+1)-intervales.get(q)>1) {
+		        	  
+		        	  System.out.println("thread numéro :" + q);
+		        	  
+		        	  
+		        		  List<String> chaine = new ArrayList<>(); 
 		        		  
-		        	  for (int u=intervales.get(q) ; u<intervales.get(q+1)+1; u++) { // on se place dans le premier intervalle
+		        	  for (int u=intervales.get(q) ; u<intervales.get(q+1)+1; u++) { 
 		        		  
 		        		  
-		        		  
-		        		  if(u<words.size()) {
+		        		  if(u<words.size()) { 
 		        			  
-		        		  chaine.add(String.valueOf(words.get(u))); // PB est ici, word va jusqu'à 100 et intervales va jusq'à 114
-		        		  compteur ++;
+		        		  chaine.add(String.valueOf(words.get(u)));
 		        			  
 		        		  
 		        		  }
 		        	  
 	        		 
 		        	  }
+		        	 
 		        	  System.out.println(chaine);
-		        	  System.out.println(compteur);
+		        	 
 		        	  chaines.add(chaine);
 		        	       	 
-		        		 
+		        	  }	 
 		          }
 		          
-		          System.out.println(chaines);
 
-		          // code trouvé sur Internet qui permet de remove 1 mot sur 2 (index impair)
-		          int i[] = {1,3,5,7,9,11,13,15};
 
-		          Iterator<List<String>> itr = chaines.iterator();
-		          int pos = 0;
-		          int index = 0;
-		          while( itr.hasNext() ){
-		              itr.next();
-		              if( pos >= i.length ){
-		                  break;
-		              }
-		              if( i[pos] == index ){
-		                  itr.remove();
-		                  pos++;
-		              } 
-
-		              index++;
-		          }
-		          
-		          // fin du code trouvé sur internet
 		          
 	      System.out.println(chaines);
 		        	 
